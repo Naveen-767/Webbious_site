8e5171783c280c8d5ca8290f42c1d932e916ff45
Naveen <test@w3schools.com>
2025-03-28 18:00:38 +0530

    Naveen commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import Header from "./components/Navbar";
+import Hero from "./components/hero";
+import Ca from "./components/c";
+import Card1 from "./components/Card1";
+import Card2 from "./components/Card2";
+import ContactUs from "./components/contact";
+import ProjectsSection from "./components/Projects";
+import Team from "./components/team";
+import Page from "./components/anmation";
 
-const App = () => {
+
+
+
+function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Webbious Starter pack</h1>
+    <div className="min-h-screen">
+      <main className="pt-16">
+        <Header />
+      </main>
+<div className="pb-16">
+      <Hero /></div>
+      {/* <Ca /> */}
+      <div className="pb-16" >     <Card1 /></div>
+
+      <Page /> 
+      <Card2 />
+      <ProjectsSection />
+      <Team />
+      <ContactUs />
+
+    
+
     </div>
   );
-};
+}
 
 export default App;
